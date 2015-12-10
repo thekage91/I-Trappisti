cf1c9efbd1f04c48a816313172a439d64dd868f4
davide <the--kage91@hotmail.it>
2015-12-10 19:10:07 +0100

    Improve stripe integration

--- js/script.custom.js
+++ js/script.custom.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
 		$('#newsletter').addClass("hidden");
 		$('.price-bicchiere').removeClass("hidden");
 		$('.price-semplice').addClass("hidden");
+		$('#pay_form_bicchiere').removeClass("hidden");
+		$('#pay_form_semplice').addClass("hidden");
 
 	});
 
@@ -16,7 +18,8 @@ $(document).ready(function(){
 		$('#newsletter').addClass("hidden");
 		$('.price-semplice').removeClass("hidden");
 		$('.price-bicchiere').addClass("hidden");
-		
+		$('#pay_form_bicchiere').addClass('hidden');
+		$('#pay_form_semplice').removeClass('hidden');
 	});
 })
 
