0342b0a2c152278d3e31ce244730b273ebe68db2
davide <the--kage91@hotmail.it>
2015-12-10 22:53:21 +0100

    Insert mail chimp for subscription

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
 		$('#content').addClass('animated fadeInUp'); 
 
 	}, 1000);*/
+
+	$('#subscribe-form').ajaxChimp({
+		language: 'it',
+	    url: 'http://passparyou.us11.list-manage.com/subscribe/post?u=baa6a96ac00514e2d994c55e2&amp;id=00137f3e7c'
+	});
+
+	$.ajaxChimp.translations.it = {
+		    'submit': '',
+		    0: "Grazie! Controlla la tua mail per confermare l'iscrizione",
+		    1: 'Per favore inserisci un valore',
+		    2: 'Una dirección de correo electrónico debe contener una sola @',
+		    3: 'Attenzione! La parte del dominio della mail non è valida',
+		    4: 'Attenzione! La parte relativa al nome utente della mail non è valida',
+		    5: "L'indirizzo email che hai inserito non è valido. Si prega di inserirne uno valido"
+}
 })
